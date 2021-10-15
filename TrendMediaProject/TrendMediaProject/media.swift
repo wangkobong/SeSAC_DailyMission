3d6cea0d1fd048975212256a49d5ea7f66b6626c
Kobong <wangkobong@gmail.com>
2021-10-15 18:46:55 +0900

    add launchScreen and media data

---
+++ TrendMediaProject/TrendMediaProject/media.swift
@@ -0,0 +1,21 @@
+//
+//  media.swift
+//  TrendMediaProject
+//
+//  Created by sungyeon kim on 2021/10/15.
+//
+
+import Foundation
+import UIKit
+
+struct TvShow {
+    let title: String
+    let releaseDate: String
+    let genre: String
+    let region: String
+    let overview: String
+    let rate: Double
+    let starring: String
+    let backdropImage: String
+}
+
