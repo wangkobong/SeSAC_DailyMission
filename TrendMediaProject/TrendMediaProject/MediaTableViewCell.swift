88f5d719e05435b2d8f8be1ae6ec1a01afd53159
Kobong <wangkobong@gmail.com>
2021-10-16 18:25:12 +0900

    add ViewController + MediaTableViewCell

---
+++ TrendMediaProject/TrendMediaProject/MediaTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MediaTableViewCell.swift
+//  TrendMediaProject
+//
+//  Created by sungyeon kim on 2021/10/16.
+//
+
+import UIKit
+
+class MediaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TrendMediaProject/TrendMediaProject/ViewController.swift
+++ TrendMediaProject/TrendMediaProject/ViewController.swift
@@ -7,12 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MediaTableViewCell") else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 500
+    }
+
 
 
 }
