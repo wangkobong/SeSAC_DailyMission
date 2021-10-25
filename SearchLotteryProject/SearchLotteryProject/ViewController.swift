807ca6d8cdd44535fa89d2d7da87d4eee49e91c0
Kobong <wangkobong@gmail.com>
2021-10-25 16:58:32 +0900

    first commit for SearchLotteryProject

---
+++ SearchLotteryProject/SearchLotteryProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchLotteryProject
+//
+//  Created by sungyeon kim on 2021/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
