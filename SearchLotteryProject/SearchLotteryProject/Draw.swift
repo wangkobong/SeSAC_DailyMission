5973be0e8e363ea6c10bd649574652a6cc97b453
Kobong <wangkobong@gmail.com>
2021-10-25 17:45:56 +0900

    add DrawModel and IBOutlets

---
+++ SearchLotteryProject/SearchLotteryProject/Draw.swift
@@ -0,0 +1,27 @@
+//
+//  Draw.swift
+//  SearchLotteryProject
+//
+//  Created by sungyeon kim on 2021/10/25.
+//
+
+import Foundation
+
+/*
+ 내가 필요한거
+ 1. 추첨날짜
+ 2. 회차
+ 3. 당첨번호 7개
+ */
+
+struct Draw {
+    let drwNoDate: String
+    let drwNo: String
+    let drwtNo1: Int
+    let drwtNo2: Int
+    let drwtNo3: Int
+    let drwtNo4: Int
+    let drwtNo5: Int
+    let drwtNo6: Int
+    let bnusNo: Int
+}
--- SearchLotteryProject/SearchLotteryProject/ViewController.swift
+++ SearchLotteryProject/SearchLotteryProject/ViewController.swift
@@ -6,12 +6,49 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+
+/*
+ 내가 필요한거
+ 1. 추첨날짜
+ 2. 회차
+ 3. 당첨번호 7개
+ */
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var drawNumberTextFiled: UITextField!
+    @IBOutlet weak var drawDateLabel: UILabel!
+    @IBOutlet weak var drawNumberLabel: UILabel!
+    
+    @IBOutlet weak var firstWinNumber: UILabel!
+    @IBOutlet weak var secondWinNumber: UILabel!
+    @IBOutlet weak var thirdWinNumber: UILabel!
+    @IBOutlet weak var fourthWinNumber: UILabel!
+    @IBOutlet weak var fifthWinNumber: UILabel!
+    @IBOutlet weak var sixthWinNumber: UILabel!
+    @IBOutlet weak var bonusWinNumber: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getDrawResult(861)
+    }
+    
+    func getDrawResult(_ drawNo: Int) {
+        
+        let url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=\(drawNo)"
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
 
