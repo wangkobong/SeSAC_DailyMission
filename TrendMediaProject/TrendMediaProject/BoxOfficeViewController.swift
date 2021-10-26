1fe41e43e9adcb867248d5e0cc26cb60461244a2
Kobong <wangkobong@gmail.com>
2021-10-27 00:37:26 +0900

    fetch boxofficedata by yesterday Date

--- TrendMediaProject/TrendMediaProject/BoxOfficeViewController.swift
+++ TrendMediaProject/TrendMediaProject/BoxOfficeViewController.swift
@@ -14,6 +14,7 @@ class BoxOfficeViewController: UIViewController, UITableViewDelegate, UITableVie
     
     @IBOutlet weak var boxOfficeTableView: UITableView!
     var boxOfficeData: [BoxOfficeModel] = []
+    var yesterdayDateToString = ""
 
     @IBOutlet weak var releaseDateTextField: TweeAttributedTextField!
     
@@ -21,13 +22,19 @@ class BoxOfficeViewController: UIViewController, UITableViewDelegate, UITableVie
         super.viewDidLoad()
         boxOfficeTableView.delegate = self
         boxOfficeTableView.dataSource = self
-        
-        
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        let yesterday = Date.yesterday
+        yesterdayDateToString = dateFormatter.string(from: yesterday)
+
+
+        print(yesterdayDateToString)
 //        releaseDateTextField.activeLineColor = .systemBlue
 //        releaseDateTextField.activeLineWidth = 1
 //        releaseDateTextField.animationDuration = 0.3
 
-        fetchBoxOfficeData()
+        fetchBoxOfficeData(yesterdayDateToString)
     }
     
 
@@ -35,9 +42,9 @@ class BoxOfficeViewController: UIViewController, UITableViewDelegate, UITableVie
         print("검색")
     }
     
-    func fetchBoxOfficeData() {
+    func fetchBoxOfficeData(_ yesterdayDate: String) {
         let boxOfficeKey = Bundle.main.boxOfficeKey
-        let url = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(boxOfficeKey)&targetDt=20211025"
+        let url = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(boxOfficeKey)&targetDt=\(yesterdayDate)"
         AF.request(url, method: .get).validate().responseJSON { response in
             switch response.result {
             case .success(let value):
@@ -50,9 +57,7 @@ class BoxOfficeViewController: UIViewController, UITableViewDelegate, UITableVie
                     self.boxOfficeData.append(data)
                 }
                 self.boxOfficeTableView.reloadData()
-                print("boxOfficeData : \(self.boxOfficeData)")
-                
-                print("JSON: \(json)")
+
             case .failure(let error):
                 print(error)
             }
@@ -73,9 +78,13 @@ class BoxOfficeViewController: UIViewController, UITableViewDelegate, UITableVie
         
         cell.movieTitleLabel.text = row.titleData
         cell.orderLabel.text = String(indexPath.row + 1)
-        cell.releaseDateLabel.text = row.releaseData
+        if row.releaseData != "" {
+            cell.releaseDateLabel.text = row.releaseData
+        } else {
+            cell.releaseDateLabel.text = "해당 정보가 존재하지 않습니다."
+        }
+
         
-        print("cell : \(cell)")
         return cell
     }
 //    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -87,3 +96,23 @@ class BoxOfficeViewController: UIViewController, UITableViewDelegate, UITableVie
     
 }
 
+// MARK: - get date
+extension Date {
+    static var yesterday: Date { return Date().dayBefore }
+    static var tomorrow:  Date { return Date().dayAfter }
+    var dayBefore: Date {
+        return Calendar.current.date(byAdding: .day, value: -1, to: noon)!
+    }
+    var dayAfter: Date {
+        return Calendar.current.date(byAdding: .day, value: 1, to: noon)!
+    }
+    var noon: Date {
+        return Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: self)!
+    }
+    var month: Int {
+        return Calendar.current.component(.month,  from: self)
+    }
+    var isLastDayOfMonth: Bool {
+        return dayAfter.month != month
+    }
+}
