55fe1026334e3d2c55d01ff9c2cd84b4b755dcad
Kobong <wangkobong@gmail.com>
2021-12-31 18:39:52 +0900

    연말과제 프로젝트 생성

---
+++ SeSAC_Community/SeSAC_Community/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_Community
+//
+//  Created by sungyeon kim on 2021/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
